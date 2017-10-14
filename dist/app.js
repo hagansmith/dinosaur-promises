696c9accdfb9c6347cef42279afd4093c8e8936a
Adam Smith <hagansmith@gmail.com>
2017-10-14 12:00:26 -0500

    updated files for project with cats, added sass styling

--- dist/app.js
+++ dist/app.js
@@ -5,134 +5,85 @@ var dom = require('./dom');
 
 var dinosaurs = [];
 
-
-// The old way - Pyramid of DOOM
-// var dinoGetter = function(){
-// 	$.ajax("./db/dinosaurs.json").done(function(data1){
-// 		console.log("data1", data1);
-// 		data1.dinosaurs1.forEach(function(dino){
-// 			dinosaurs.push(dino);
-// 		});
-// 		$.ajax("./db/dinosaurs2.json").done(function(data2){
-// 			console.log("data2", data2);
-// 			data2.dinosaurs2.forEach(function(dino){
-// 				dinosaurs.push(dino);
-// 			});
-// 			$.ajax("./db/dinosaurs3.json").done(function(data3){
-// 				console.log("data3", data3);
-// 				data3.dinosaurs3.forEach(function(dino){
-// 					dinosaurs.push(dino);
-// 				});
-// 				console.log("dinosaurs after foreach", dinosaurs);
-// 			});
-// 		});
-// 	});
-// };
-
-var firstDinosaurJSON = function(){
-	return new Promise(function(resolve, reject){
-		$.ajax('./db/dinosaurs.json').done(function(data1){
+const firstDinosaurJSON = () => {
+	return new Promise((resolve, reject) => {
+		$.ajax('./db/dinosaurs.json').done((data1) => {
 			resolve(data1.dinosaurs1);
-		}).fail(function(error1){
+		}).fail((error1) => {
 			reject(error1);
 		});
 	});
 };
 
-var secondDinosaurJSON = function(){
-	return new Promise(function(resolve, reject){
-		$.ajax('./db/dinosaurs2.json').done(function(data2){
+const secondDinosaurJSON = () => {
+	return new Promise((resolve, reject) => {
+		$.ajax('./db/dinosaurs2.json').done((data2) => {
 			resolve(data2.dinosaurs2);
-		}).fail(function(error2){
+		}).fail((error2) => {
 			reject(error2);
 		});
 	});
 };
 
-var thirdDinosaurJSON = function(){
-	return new Promise(function(resolve, reject){
-		$.ajax('./db/dinosaurs3.json').done(function(data3){
+const thirdDinosaurJSON = () => {
+	return new Promise((resolve, reject) => {
+		$.ajax('./db/dinosaurs3.json').done((data3) => {
 			resolve(data3.dinosaurs3);
-		}).fail(function(error3){
+		}).fail((error3) => {
 			reject(error3);
 		});
 	});
 };
 
-
-// PROMISE WORKS - promise pyramid of DOOM
-// var dinoGetter = function(){
-// 	firstDinosaurJSON().then(function(results){
-// 		results.forEach(function(dino){
-// 			dinosaurs.push(dino);
-// 		});
-// 		secondDinosaurJSON().then(function(results2){
-// 			results2.forEach(function(dino){
-// 				dinosaurs.push(dino);
-// 			});
-// 			thirdDinosaurJSON().then(function(results3){
-// 				results3.forEach(function(dino){
-// 					dinosaurs.push(dino);
-// 				});
-// 				console.log("dinosaurs", dinosaurs);
-// 			});
-// 		});
-// 	}).catch(function(error){
-// 		console.log("error", error);
-// 	});
-// };
-
-
-//Promise - Correct Way
-// var dinoGetter = function(){
-// 	firstDinosaurJSON().then(function(results){
-// 		results.forEach(function(dino){
-// 			dinosaurs.push(dino);
-// 		});
-// 		return secondDinosaurJSON();
-// 	}).then(function(results2){
-// 		results2.forEach(function(dino){
-// 			dinosaurs.push(dino);
-// 		});
-// 		return thirdDinosaurJSON();
-// 	}).then(function(results3){
-// 		results3.forEach(function(dino){
-// 			dinosaurs.push(dino);
-// 		});
-// 		console.log("dinosaurs", dinosaurs);
-// 		makeDinos();
-// 	});
-// };
-
-var dinoGetter = function(){
-	Promise.all([firstDinosaurJSON(), secondDinosaurJSON(), thirdDinosaurJSON()]).then(function(results){
-		console.log("results from promise.all", results);
-		results.forEach(function(result){
-			result.forEach(function(dino){
-				dinosaurs.push(dino);
-			});
+const allTheCats = () => {
+	return new Promise((resolve, reject) => {
+		$.ajax('./db/cats.json').done((catData) => {
+			resolve(catData.cats);
+		}).fail((error) => {
+			reject(error);
 		});
-		makeDinos();
-	}).catch(function(error){
+	});
+};
+
+const dinoGetter = () => {
+	Promise.all([firstDinosaurJSON(), secondDinosaurJSON(), thirdDinosaurJSON()]).then((results) => {
+		allTheCats().then((cats) =>{
+			results.forEach((result) => {
+				result.forEach((dino) => {
+					dino.snacks = [];
+					dino.catIds.forEach((catId) =>{
+						cats.forEach((cat) => {
+							if(cat.id === catId){
+								dino.snacks.push(cat);
+							}
+						});
+					});
+					dinosaurs.push(dino);
+				});
+			});
+			makeDinos();
+		});
+	}).catch((error) => {
 		console.log("error from Promise.all", error);
 	});
 };
 
-var makeDinos = function(){
-	dinosaurs.forEach(function(dino){
+const makeDinos = () => {
+	dinosaurs.forEach((dino) => {
 		dom(dino);
 	});
 };
 
-var initializer = function(){
-	dinoGetter();	
+const initializer = () => {
+	dinoGetter();
 };
 
-var getDinosaurs = function(){
+const getDinosaurs = () => {
 	return dinosaurs;
 };
 
 module.exports = {initializer:initializer, getDinosaurs:getDinosaurs};
+
 },{"./dom":2}],2:[function(require,module,exports){
 "use strict";
 
@@ -140,9 +91,26 @@ var outputDiv = $('#dinosaurs');
 
 var domString = function(dinosaur) {
 	var domStrang = '';
-      domStrang += `<div>`;
+      domStrang += `<div class=${dinosaur.info === 'Carnivore' ? 'card-bad' : 'card-good'}>`;
       domStrang +=   `<h1>${dinosaur.type}</h1>`;
+			domStrang +=   `<h4>${dinosaur.bio}</h4>`;
+			if (dinosaur.info === 'Carnivore') {
+				domStrang += `<h4>Has some tasty snacks.</h4>`;
+			} else {
+				domStrang += `<h4>Has some adorable friends.</h4>`;
+			}
+			domStrang += `<div class='card-holder'>`;
+			dinosaur.snacks.forEach((cat) => {
+				domStrang += `<div class='card'>`;
+				domStrang += 	`<h5>${cat.name}</h5>`;
+				domStrang += 	`<div class='card-img'>`;
+				domStrang += 		`<img src=${cat.imageUrl}>`;
+				domStrang += 	`</div>`;
+				domStrang += 	`<p class='card-description'>${cat.specialSkill}</p>`;
+				domStrang += `</div>`;
+			});
       domStrang += `</div>`;
+			domStrang += `</div>`;
 	printToDom(domStrang);
 };
 
